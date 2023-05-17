6453275bbede1d058040389f2bc9a69fe01017c0
Rj-Matic <rjmatic19@gmail.com>
2023-05-17 18:53:57 +0800

    updated

--- src/Components/pages/Login.js
+++ src/Components/pages/Login.js
@@ -1,7 +1,54 @@
-import React from "react";
-
+import classes from "../css/Login.module.css";
+import { useState } from "react";
+import { Form } from "react-router-dom";
+import { AiOutlineUser, AiOutlineLock, AiOutlineUnlock } from "react-icons/ai";
+// image
+import icon from "../image/UDMZ.png";
+import logo from "../image/lion.png";
 const Login = () => {
-  return <div>Login</div>;
+  const [isPassword, setIsPassword] = useState(false);
+
+  const showPassword = () => {
+    setIsPassword(!isPassword);
+  };
+  return (
+    <div className={classes.Wrapper}>
+      <div className={classes.Container}>
+        <img src={icon} alt="icon" />
+        <Form>
+          <img src={logo} alt="icon" id={classes.lion_Logo} />
+          <span id={classes.heading}>LOGIN</span>
+          <div>
+            <AiOutlineUser size="2rem" id={classes.LoginIcon} />
+            <input type="text" placeholder="Username" name="username" />
+          </div>
+
+          <div>
+            {!isPassword ? (
+              <AiOutlineLock
+                size="2rem"
+                id={classes.LoginIcon}
+                onClick={showPassword}
+              />
+            ) : (
+              <AiOutlineUnlock
+                size="2rem"
+                id={classes.LoginIcon}
+                onClick={showPassword}
+              />
+            )}
+            <input
+              type={isPassword ? "text" : "password"}
+              placeholder="password"
+              name="password"
+            />
+          </div>
+          <span id={classes.forgot_Password}>Forgot Password?</span>
+          <button id={classes.btn}>LOGIN</button>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
